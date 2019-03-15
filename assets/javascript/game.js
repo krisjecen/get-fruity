5d49dc1489897e5e8328aaa0ea9b38ded75eeec3
On Kilter <13891670+krisjecen@users.noreply.github.com>
2019-03-14 22:59:41 -0700

    updated js with letter check and eliminated two variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,26 @@
 //  very simple arrays of one letter and one underscore to represent the puzzle environment
+"use strict"
 
 var blankSet = ["_"];
 var wordSet = ["a"];
 var wrongLetters = [];
 
 // targeting the location for displaying the current word
-var targetDiv = document.getElementById("currentWord");
+// var targetDiv = ; could take this out if i can just insert text into the document
+// same thing happened below, we were able to remove the wrongDiv variable like we
+// removed the targetDiv variable
 // creating a div element in the current word area
 var gameArea = document.createElement("div");
 // inserting text content, in this simplified case, one underscore
 gameArea.textContent = blankSet;
-targetDiv.appendChild(gameArea);
+document.getElementById("currentWord").appendChild(gameArea);
 
 // targeting the location for displaying guessed letters that aren't in the current word
-var wrongDiv = document.getElementById("wrongLetters");
+//var wrongDiv = ;
 // creating a div element in the area for letters guessed
 var letterGuessed = document.createElement("div");
-letterGuessed.textContent = ["wrong guesses appear here"];
-wrongDiv.appendChild(letterGuessed);
+letterGuessed.textContent = [`You've guessed: `];
+document.getElementById("wrongLetters").appendChild(letterGuessed);
 
 // when the user presses a key, if that key is the letter "a", then reveal the "a"
 // otherwise leave the blank / underscore and console log "there is no [key pressed] in the word"
@@ -25,21 +28,27 @@ wrongDiv.appendChild(letterGuessed);
 
 document.onkeyup = (event) => {
     // consider making an if statement about if the key pressed is a letter key
-    // look up javascript keycodes & ask about it
-    
-    var userPlay = event.key;
-    //alert(`You played: ${userPlay}`);
-    console.log(userPlay);
+    // look up javascript keycodes & look in slack resources
+    var userPlay = event.key.toLowerCase();
 
-    if (userPlay === "a") {
-        gameArea.textContent = wordSet;
-    }
-    else {
-        // look up the .push() 
-        wrongLetters.push(` ${userPlay}`);
-        letterGuessed.textContent = `You've guessed: ${wrongLetters}`;
-        console.log(`wrong letters: ${wrongLetters}`);
-    
+    // this helped in not displaying F5 ^_^;;
+    if ("abcdefghijklmnopqrstuvwxyz".includes(userPlay)) {
+        console.log(userPlay);
+
+        if (userPlay === "a") {
+            gameArea.textContent = wordSet;
+        }
+        else {
+            // look up the .push() 
+            // add an if to exclude letters already guessed from getting added again
+
+            // add the wrong guess to the wrongLetters array (now without the extra space!)
+            wrongLetters.push(`${userPlay}`);
+            // the letterGuessed text area gets updated with the new wrong guess (itself plus the new guess)
+            letterGuessed.textContent += `${userPlay}, `;
+            console.log(`wrong letters: ${wrongLetters}`);
+        
+        }
     }
     
 }
