3aad25c709e536e0d0f04fffffd017d03497832f
On Kilter <13891670+krisjecen@users.noreply.github.com>
2019-03-15 09:44:38 -0700

    repeat guesses no longer affect countdown; added last chance reminder

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,13 @@
 //  very simple arrays of one letter and one underscore to represent the puzzle environment
 "use strict"
 
+// initialize variables
+
 var blankSet = ["_"];
 var wordSet = ["a"];
 var wrongLetters = [];
+var winsCount = 0;
+var remChances = 12;
 
 // targeting the location for displaying the current word
 // var targetDiv = ; could take this out if i can just insert text into the document
@@ -15,11 +19,17 @@ var gameArea = document.createElement("div");
 gameArea.textContent = blankSet;
 document.getElementById("currentWord").appendChild(gameArea);
 
+// remaining guesses/chances -- inserting text content
+var remaining = document.createElement("div");
+remaining.textContent = (`Chances: ${remChances}`);
+document.getElementById("remainingGuesses").appendChild(remaining);
+
+
 // targeting the location for displaying guessed letters that aren't in the current word
 //var wrongDiv = ;
 // creating a div element in the area for letters guessed
 var letterGuessed = document.createElement("div");
-letterGuessed.textContent = [`You've guessed: `];
+letterGuessed.textContent = (`You've guessed: `);
 document.getElementById("wrongLetters").appendChild(letterGuessed);
 
 // when the user presses a key, if that key is the letter "a", then reveal the "a"
@@ -31,21 +41,52 @@ document.onkeyup = (event) => {
     // look up javascript keycodes & look in slack resources
     var userPlay = event.key.toLowerCase();
 
-    // this helped in not displaying F5 ^_^;;
+    // check that the userPlay is a letter, not some other key. this helped in not displaying F5 ^_^;;
     if ("abcdefghijklmnopqrstuvwxyz".includes(userPlay)) {
         console.log(userPlay);
 
+        // does the userplay match the current word?
         if (userPlay === "a") {
             gameArea.textContent = wordSet;
         }
         else {
-            // look up the .push() 
-            // add an if to exclude letters already guessed from getting added again
+            // exclude letters already guessed from getting added again
+            if (wrongLetters.includes(userPlay) == false) {
+                
+                // add the wrong guess to the wrongLetters array (now without the extra space!)
+                wrongLetters.push(`${userPlay}`);
+                // the letterGuessed text area gets updated with the new wrong guess (itself plus the new guess)
+                letterGuessed.textContent += `${userPlay}, `;
+                remChances -= 1;
+                remaining.textContent = (`Chances: ${remChances}`);
 
-            // add the wrong guess to the wrongLetters array (now without the extra space!)
-            wrongLetters.push(`${userPlay}`);
-            // the letterGuessed text area gets updated with the new wrong guess (itself plus the new guess)
-            letterGuessed.textContent += `${userPlay}, `;
+            
+                // if the player gets down to 1 chance, add a "last chance!" reminder
+                if (remChances === 1) {
+                    remaining.textContent = (`Chances: ${remChances} -- this is your last chance!`);
+                    
+                }
+
+
+            // if the player runs out of chances, what do we do?
+                if (remChances === 0) {
+                    // the line below doesn't display as Chances: 0 at all  ._.
+                    remaining.textContent = (`Chances: ${remChances}`);
+                    // tell them that the game is over
+                    console.log("game over :(");
+                    // reset the chances to 12 so they can play again
+                    remChances = 12;
+                    remaining.textContent = (`Chances: ${remChances}`);
+                    // assign a new word to the current word slot (underscores)
+                    gameArea.textContent = blankSet;
+                    // reset the wrong guesses area
+                    letterGuessed.textContent = (`You've guessed: `);
+                    // clear out the wrongLetters array
+                    wrongLetters = [];
+                    
+                }
+            }
+            console.log(remChances);
             console.log(`wrong letters: ${wrongLetters}`);
         
         }
